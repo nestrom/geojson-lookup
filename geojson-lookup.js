3318b71a69562e99ed052a17615bdde675e3f0de
Anas Alhamdan <cis.ash@gmail.com>
2017-02-13 17:23:23 +0200

    cleaning up and review
    some cleaning up, same functionality.

--- geojson-lookup.js
+++ geojson-lookup.js
@@ -2,45 +2,32 @@ var PolygonLookup = require( 'polygon-lookup' );
 
 function buildIndex(properties, geojs)
 {
-    var indexTable = {};
-    for(z in properties)
-    {
-        //console.log(indexTable);
-        if(!indexTable.hasOwnProperty(properties[z]))
-            indexTable[properties[z]] = {};
-
-        for(x in geojs.features)
-        {
-            if(!indexTable[properties[z]].hasOwnProperty(geojs.features[x].properties[properties[z]])) {
-                indexTable[properties[z]][geojs.features[x].properties[properties[z]]] = []
-            }
-
-            indexTable[properties[z]][geojs.features[x].properties[properties[z]]].push(x);
-        }
-
-    }
+    properties = properties || [];
+    var indexTable = properties.reduce((acc, val) => {
+        acc[val] = {};
+        return acc;
+    }, {});
+    
+    properties.map((prop) => {
+        geojs.features.map((feature, key) => {
+            indexTable[prop][feature.properties[prop]] = indexTable[prop][feature.properties[prop]] || [];
+            indexTable[prop][feature.properties[prop]].push(key);
+        });
+    });
+    
     indexTable['_geoLookup'] = new PolygonLookup( geojs );
-
-
     return indexTable;
 }
 
 var getCollection = {
-
+    
     byProperty: (property, value, geojs, indexTable) => {
-        var ids = indexTable[property][value];
-        var result = [];
-        for(x in ids)
-        {
-            result.push(geojs.features[ids[x]]);
-        }
-        return result;
+        return indexTable[property][value].map((id) => geojs.features[id] )
     },
-    byPoint: (lat, long, indexTable)=>{
+    byPoint: (lat, long, indexTable) => {
         return indexTable._geoLookup.searchForMultiplePolygons(long, lat, -1);
     }
 }
-
 
 module.exports.buildIndex = buildIndex;
 module.exports.getCollection = getCollection;
